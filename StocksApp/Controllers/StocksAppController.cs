9ed52c6ad2067c56ce6f7f52bf794c1f8b478716
OmarHamdyx <zoltarscorpio@gmail.com>
2024-03-31 02:50:56 +0200

    Code Update

--- StocksApp/Controllers/StocksAppController.cs
+++ StocksApp/Controllers/StocksAppController.cs
@@ -3,26 +3,27 @@ using Interfaces;
 using Models;
 namespace StocksApp.Controllers
 {
-	[Route("[controller]")]
-	public class StocksAppController : Controller
-	{
-		private readonly IGetStockModelViewService _getStockModelView;
+    [Route("[controller]")]
+    public class StocksAppController : Controller
+    {
+        private readonly IGetStockModelViewService _getStockModelView;
         private readonly IConfiguration _configuration;
 
-        public StocksAppController(IGetStockModelViewService getStockModelView,IConfiguration configuration)
-		{
-			_getStockModelView = getStockModelView;
+        public StocksAppController(IGetStockModelViewService getStockModelView, IConfiguration configuration)
+        {
+            _getStockModelView = getStockModelView;
             _configuration = configuration;
         }
 
-		[Route("[action]")]
+        [Route("[action]")]
+        [Route("/[controller]")]
         [Route("/[controller]/{symbol}")]
-        public async Task <IActionResult> GetStockDetails(string? symbol)
-		{
-			ViewBag.Token=_configuration["finnhubapikey"];
-			StockDetailsViewModel stockDetailsViewModel = await _getStockModelView.GetStockDetailsViewModel(symbol);
-		
-			return View(stockDetailsViewModel);
-		}
-	}
+        public async Task<IActionResult> GetStockDetails(string? symbol)
+        {
+            ViewBag.Token = _configuration["finnhubapikey"];
+            StockDetailsViewModel stockDetailsViewModel = await _getStockModelView.GetStockDetailsViewModel(symbol);
+
+            return View(stockDetailsViewModel);
+        }
+    }
 }
