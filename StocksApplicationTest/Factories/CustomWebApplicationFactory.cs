35f0459395615c7f38c8e959ada9bfd57779f23f
OmarHamdyx <o.hamdy1999@gmail.com>
2024-06-10 01:30:29 +0300

    Tested GetOrders using Integration Testing.

---
+++ StocksApplicationTest/Factories/CustomWebApplicationFactory.cs
@@ -0,0 +1,45 @@
+﻿using Application.Interfaces;
+using Infrastructure.DbContexts;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace StocksApplicationTest.Factories
+{
+	public class CustomWebApplicationFactory : WebApplicationFactory<Program>
+	{
+		public Mock<IStocksService> StocksServiceMock { get; private set; }
+
+		protected override void ConfigureWebHost(IWebHostBuilder builder)
+		{
+			builder.UseEnvironment("Test");
+
+			builder.ConfigureServices(services =>
+			{
+				var descriptor = services.SingleOrDefault(d => d.ServiceType == typeof(DbContextOptions<MsSqlServerDbContext>));
+				if (descriptor != null)
+				{
+					services.Remove(descriptor);
+				}
+
+				services.AddDbContext<MsSqlServerDbContext>(options =>
+				{
+					options.UseInMemoryDatabase("DatabaseForTesting");
+				});
+
+				var stocksServiceDescriptor = services.SingleOrDefault(d => d.ServiceType == typeof(IStocksService));
+				if (stocksServiceDescriptor != null)
+				{
+					services.Remove(stocksServiceDescriptor);
+				}
+
+				StocksServiceMock = new Mock<IStocksService>();
+				services.AddSingleton(StocksServiceMock.Object);
+			});
+
+			base.ConfigureWebHost(builder);
+		}
+	}
+}
---
+++ StocksApplicationTest/IntegrationTests/StocksAppIntegrationTest.cs
@@ -0,0 +1,58 @@
+﻿using FluentAssertions;
+using HtmlAgilityPack;
+using StocksApplicationTest.Factories;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xunit;
+using AutoFixture;
+using Application.DtoModels;
+using Moq;
+using Application.Interfaces;
+
+namespace StocksApplicationTest.IntegrationTests
+{
+	public class StocksAppIntegrationTest : IClassFixture<CustomWebApplicationFactory>
+	{
+		private readonly HttpClient _client;
+		private readonly CustomWebApplicationFactory _factory;
+		private readonly Fixture _fixture;
+
+
+		public StocksAppIntegrationTest(CustomWebApplicationFactory factory)
+		{
+			_factory = factory;
+			_client = factory.CreateClient();
+			_fixture = new Fixture();
+		}
+
+		[Fact]
+		public async Task GetOrders_ToReturnView()
+		{
+			// Arrange
+			var buyOrders = _fixture.CreateMany<BuyOrderResponse>(10).ToList();
+			var sellOrders = _fixture.CreateMany<SellOrderResponse>(10).ToList();
+
+			_factory.StocksServiceMock.Setup(service => service.GetBuyOrdersAsync())
+									  .ReturnsAsync(buyOrders);
+
+			_factory.StocksServiceMock.Setup(service => service.GetSellOrdersAsync())
+									  .ReturnsAsync(sellOrders);
+
+			// Act
+			HttpResponseMessage response = await _client.GetAsync("/StocksApp/GetOrders");
+
+			// Assert
+			response.Should().BeSuccessful(); // Check if response status is 2xx
+
+			string responseBody = await response.Content.ReadAsStringAsync();
+
+			HtmlDocument html = new HtmlDocument();
+			html.LoadHtml(responseBody);
+			var document = html.DocumentNode;
+
+			var ordersDiv = document.SelectSingleNode("//div[contains(@class, 'orders')]");
+			ordersDiv.Should().NotBeNull();
+		}
+	}
+}
